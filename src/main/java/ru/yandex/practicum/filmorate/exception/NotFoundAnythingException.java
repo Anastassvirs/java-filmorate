9dd2e3f3bfa1fba10f096da477eed6f37fa89748
Анастасия Свирская <foreducationYa@yandex.ru>
2022-11-02 14:08:23 +0300

    fix: Удалены лишние конструкторы в классах ошибок

--- src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundAnythingException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundAnythingException.java
@@ -1,10 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
 public class NotFoundAnythingException extends RuntimeException{
-    public NotFoundAnythingException() {
-        super();
-    }
-
     public NotFoundAnythingException(final String message) {
         super(message);
     }
